1ab962b828e93698784fe5549b9f777d994f14dd
Alex Ly <alexly@nbp-51-219.nbp.ruw.rutgers.edu>
2016-02-04 21:08:58 -0500

    Added basic middleware

--- exercise1/exercise1.js
+++ exercise1/exercise1.js
@@ -3,8 +3,18 @@ var bodyParser = require('body-parser');
 var app = express();
 var PORT = 8080;
 
+function myLoggingMiddleware(req, res, next){
+  var url = req.url;
+  var method = req.method;
+
+  console.log('%s request at %s',  method, url);
+  next();
+}
+
 // This is the bodyParser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(myLoggingMiddleware);
 
 app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
